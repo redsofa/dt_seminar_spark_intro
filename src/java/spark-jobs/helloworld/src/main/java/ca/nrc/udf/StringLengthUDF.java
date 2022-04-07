3c3e6d6921744f15c507ca3cda35dde8b029ebf0
Rene Richard <rene.richard@nrc.ca>
2022-04-07 11:34:11 -0300

    Notebook and Java code updates

--- src/java/spark-jobs/helloworld/src/main/java/ca/nrc/udf/StringLengthUDF.java
+++ src/java/spark-jobs/helloworld/src/main/java/ca/nrc/udf/StringLengthUDF.java
@@ -3,8 +3,8 @@ package ca.nrc.udf;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.api.java.UDF1;
 
+// Note that the "UDF1<T1,R>"" super interface is "java.io.Serializable"
 public class StringLengthUDF implements UDF1<String, Integer> {
-
     @Override
     public Integer call(String str) throws Exception {
         if(str != null && !str.trim().isEmpty()){
